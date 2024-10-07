b82b4315249986430512c249aa1ef29aa6550f31
Halil İbrahim Sandıraz <halilsan1994@gmail.com>
2024-10-07 12:07:08 +0300

    Project

---
+++ app.py
@@ -0,0 +1,72 @@
+from flask import Flask, render_template, request, send_from_directory, redirect, url_for, after_this_request
+from werkzeug.utils import secure_filename
+import os
+from ultralytics import YOLO
+import yolov8_segmentation as bruh
+import cv2
+import shutil
+import http
+
+
+
+model = YOLO("yolov8X-seg.pt")
+
+app = Flask(__name__)
+
+upload_folder = os.path.join("static", 'uploads')
+ 
+app.config['UPLOAD'] = upload_folder
+app.config['BGFOLDER'] = os.path.join("static", "bgremoved")
+app.config['CACHE'] = os.path.join("static", "bgremoved", "cache")
+
+bgremoverpage = "/"
+ 
+@app.route(bgremoverpage, methods=['GET', 'POST'])
+def upload_file():
+    
+    if request.method == 'POST':
+        
+        file = request.files['img']
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD'], filename))
+        filepath = bruh.getpersonsegmentation(model=model, imagepath=os.path.join(app.config['UPLOAD'], filename), destination=app.config['BGFOLDER'])
+        print(filepath)
+        if not filepath:
+            print("it is in there")
+            return render_template('image_render.html', noperson=True)
+        bruh.delete_all_png_files(app.config["CACHE"])
+        bruh.checktime(app.config['BGFOLDER'], 5)
+        return render_template('image_render.html', img=filepath)
+    return render_template('image_render.html')
+
+@app.route(f'{bgremoverpage}?<string:filepath>', methods=['POST'])
+def makechange(filepath):
+    path = os.path.join(app.config["BGFOLDER"], filepath)
+    dest = os.path.join(app.config["CACHE"], filepath)
+    checkbox_value = request.form.get("mycheckbox")
+    color_value = request.form.get("rgb")
+    if checkbox_value == "true":
+        shutil.copy2(path, dest)
+    else:
+        bruh.changebgcolor(path, color_value, dest)
+    return redirect(url_for('download', filepath=filepath))
+
+@app.route('/static/bgremoved/cache/<filepath>', methods=['GET', 'POST'])
+def download(filepath):
+    return send_from_directory(app.config["CACHE"], filepath, as_attachment=True)
+
+@app.errorhandler(400)
+def handle_bad_request(_):
+    return render_template("badrequest.html"), 400
+
+@app.errorhandler(404)
+def noaddress(e):
+    return render_template("handler.html"), 404
+
+@app.errorhandler(Exception)
+def ehandler(e):
+    return render_template("exception.html", e=e)
+ 
+ 
+if __name__ == '__main__':
+    app.run(debug=True, port=8080, host="0.0.0.0")
---
+++ yolov8_segmentation.py
@@ -0,0 +1,86 @@
+import cv2
+import numpy as np
+import os
+import time
+
+data_sitekey="6Lfx8VcqAAAAAN8tmiS47iegq9Ga0nEXDrdaL0D4"
+hchapta_secret_key = "ES_831df69666c34a1ea2eaa6d57f88e278"
+hcaptcha_sitekey = "2a2a5a5c-ff3e-40f4-8c58-c87e89945486"
+
+def checktime(loc, minutes):
+    timer = int(time.time())
+    for i in [i for i in os.listdir(loc) if i.endswith(".txt")]:
+        file_p = os.path.join(loc, i)
+        with open(file_p, "r") as file:
+            time2 = file.read()
+        if timer - int(time2) >= minutes * 60:
+            os.remove(file_p)
+            os.remove(file_p[:-3] + "png")
+
+def getpersonsegmentation(model, imagepath, destination):
+    image = cv2.imread(imagepath) 
+    results = model(image, verbose=False, save=True, conf=0.6)  
+    kernel = np.ones((7, 7), np.uint8)  
+    try:
+        masks = results[0].masks.data.cpu().numpy() 
+    except:
+        return False
+    
+    classes = results[0].boxes.cls.cpu().numpy()
+    person_class_id = 0 
+
+    # find 'person' class
+    person_mask_indices = [i for i, cls in enumerate(classes) if int(cls) == person_class_id]
+    person_masks = [masks[i] for i in person_mask_indices]
+    # If person not detected
+    if person_masks:
+        combined_mask = np.zeros_like(person_masks[0]) 
+        for mask in person_masks:
+            combined_mask = np.logical_or(combined_mask, mask > 0.5)  
+    else:
+        return False
+
+    binary_mask = combined_mask.astype(np.uint8)
+    binary_mask_resized = cv2.resize(binary_mask, (image.shape[1], image.shape[0]), interpolation=cv2.INTER_LANCZOS4)
+    
+    
+    binary_mask_resized = cv2.erode(binary_mask_resized, kernel=kernel, iterations=4)
+    binary_mask_resized = cv2.GaussianBlur(binary_mask_resized, (151, 151), 0)
+    binary_mask_resized = binary_mask_resized[:, :, np.newaxis]  
+
+    
+    rgba_image = cv2.cvtColor(image, cv2.COLOR_BGR2BGRA) 
+    rgba_image[:, :, 3] = binary_mask_resized[:, :, 0] * 255
+
+    
+    rgba_image[rgba_image[:, :, 3] == 0] = [0, 0, 0, 0]
+    os.remove(imagepath)
+    filename = os.path.join(destination, os.path.basename(imagepath).split(".")[0] + ".png")
+    cv2.imwrite(filename, rgba_image)
+    with open(filename[:-3] + "txt", "w") as file:
+        file.write(str(int(time.time())))
+    return filename.replace("\\", "/")
+
+def hex_to_rgb(value):
+    value = value.lstrip('#')
+    lv = len(value)
+    return list(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+
+def changebgcolor(pngimagepath:str, hex:str, destination:str):
+    pngimage = cv2.imread(pngimagepath, cv2.IMREAD_UNCHANGED)
+    r, g, b = hex_to_rgb(hex)
+    rgbcode = [b, g, r]
+    pngimage[pngimage[:, :, 3] == 0] = rgbcode + [255]
+    bgrimage = cv2.cvtColor(pngimage, cv2.COLOR_BGRA2BGR)
+    cv2.imwrite(destination, bgrimage)
+
+def delete_all_png_files(folder_path):
+    
+    for filename in os.listdir(folder_path):
+        
+        if filename.endswith(".png"):
+            file_path = os.path.join(folder_path, filename)
+            try:
+                os.remove(file_path)
+            except Exception as e:
+                ...
